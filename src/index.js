279a8933ad5ca31bcec5c9ab8a28dc11da5d0e9d
Jahanvi <jahanvimp128@gmail.com>
2023-08-18 01:03:25 +0530

    components

--- src/index.js
+++ src/index.js
@@ -4,10 +4,57 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const pizzaData = [{
+  name: "Focaccia",
+  ingredients: "Bread with italian olive oil",
+  price: 6,
+  photoName: "imgs/Focaccia.jpg",
+  soldOut: false
+}, {
+  name: 'Margherita',
+  ingredients: 'Cheese and mozzarella cheese',
+  price: 5,
+  photoName: "imgs/pizza-margarita.jpg",
+  soldOut: true
+}, {
+  name: "Pizza Spinaci",
+  ingredients: "Spinach, Italian sausage, mushrooms, onions, bacon, pepperoni, pineapple",
+  price: 10,
+  photoName: "imgs/Pizza Spinaci.jpg",
+  soldOut: false
+}, {
+  name: "Pizza Funghi",
+  ingredients: "Funghi, Italian sausage, mushrooms, onions, bacon",
+  price: 8,
+  photoName: "imgs/Pizza Funghi.jpg",
+  soldOut: false
+}, {
+  name: "Pizza Salamino",
+  ingredients: "Sliced salmon, Italian sausage, onions, bacon",
+  price: 9,
+  photoName: "imgs/pizza salamino.jpg",
+  soldOut: true
+}, {
+  name: "Pizza Prosciutto",
+  ingredients: "Prosciutto, Italian sausage, onions, bacon, pepperoni",
+  price: 12,
+  photoName: "imgs/Pizza Prosciutto.jpg",
+  soldOut: true
+}]
+
+function Pizza() {
+  return <div>
+    <img src="imgs/prosciutto.jpg" alt='prosciutto'/>
+    <h2>Pizza Prosciutto</h2>
+    <p>Prosciutto, Italian sausage, onions, bacon, pepperoni</p>
+  </div>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Pizza/>
   </React.StrictMode>
 );
 
