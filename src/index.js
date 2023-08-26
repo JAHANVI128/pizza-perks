b1a9dd2b6018f57751a56b4829226f32221372ca
Jahanvi <jahanvimp128@gmail.com>
2023-08-27 00:07:51 +0530

    pizza css

--- src/index.js
+++ src/index.js
@@ -9,37 +9,37 @@ const pizzaData = [{
   name: "Focaccia",
   ingredients: "Bread with italian olive oil",
   price: 6,
-  photoName: "imgs/Focaccia.jpg",
+  photoName: "imgs/focaccia.jpg",
   soldOut: false
 }, {
   name: 'Margherita',
   ingredients: 'Cheese and mozzarella cheese',
   price: 5,
-  photoName: "imgs/pizza-margarita.jpg",
+  photoName: "imgs/margherita.jpg",
   soldOut: true
 }, {
   name: "Pizza Spinaci",
   ingredients: "Spinach, Italian sausage, mushrooms, onions, bacon, pepperoni, pineapple",
   price: 10,
-  photoName: "imgs/Pizza Spinaci.jpg",
+  photoName: "imgs/spinaci.jpg",
   soldOut: false
 }, {
   name: "Pizza Funghi",
   ingredients: "Funghi, Italian sausage, mushrooms, onions, bacon",
   price: 8,
-  photoName: "imgs/Pizza Funghi.jpg",
+  photoName: "imgs/funghi.jpg",
   soldOut: false
 }, {
   name: "Pizza Salamino",
   ingredients: "Sliced salmon, Italian sausage, onions, bacon",
   price: 9,
-  photoName: "imgs/pizza salamino.jpg",
+  photoName: "imgs/salamino.jpg",
   soldOut: true
 }, {
   name: "Pizza Prosciutto",
   ingredients: "Prosciutto, Italian sausage, onions, bacon, pepperoni",
   price: 12,
-  photoName: "imgs/Pizza Prosciutto.jpg",
+  photoName: "imgs/prosciutto.jpg",
   soldOut: true
 }]
 
@@ -80,9 +80,43 @@ function Header() {
 
 function Menu() {
   return (
-    <div className='menu'>
+    <main className='menu'>
       <h2>Our Menu</h2>
-      <Pizza />
+      <Pizza name="Focaccia" ingredients="Bread with italian olive oil" photoName="imgs/focaccia.jpg" price="6"/>
+      <Pizza name="Margherita" ingredients="Cheese and mozzarella cheese" photoName="imgs/margherita.jpg" price="5"/>
+    </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className='pizza'>
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+      </div>
+
+      {/* <img src="imgs/margherita.jpg" alt='margherita' />
+      <h3>Margherita</h3>
+      <p>Cheese and mozzarella cheese</p>
+
+      <img src="imgs/spinaci.jpg" alt='spinaci' />
+      <h3>Spinaci</h3>
+      <p>Spinach, Italian sausage, mushrooms, onions, bacon, pepperoni, pineapple</p>
+
+      <img src="imgs/funghi.jpg" alt='funghi' />
+      <h3>Funghi</h3>
+      <p>Funghi, Italian sausage, mushrooms, onions, bacon</p>
+
+      <img src="imgs/salamino.jpg" alt='salamino' />
+      <h3>Salamino</h3>
+      <p>Sliced salmon, Italian sausage, onions, bacon</p>
+
+      <img src="imgs/prosciutto.jpg" alt='prosciutto' />
+      <h3>Pizza Prosciutto</h3>
+      <p>Prosciutto, Italian sausage, onions, bacon, pepperoni</p> */}
     </div>
   );
 }
@@ -92,7 +126,7 @@ function Footer() {
   const hour = new Date().getHours();
   // console.log(hour);
 
-  let openhr = 12;
+  let openhr = 11;
   let closehr = 22;
   let isOpen = hour >= openhr && hour <= closehr;
 
@@ -102,17 +136,15 @@ function Footer() {
   // }else{
   //   alert("We are currently closed for the day !!!");
   // }
-  return <footer> {new Date().toLocaleTimeString()} We are currently open!!</footer>
+  return (
+    <footer className='footer'> 
+      {
+        new Date().toLocaleTimeString()
+      } We are currently open!!
+    </footer>
+  );
 
   // return React.createElement('footer',null,"We are currently open!!")
-}
-
-function Pizza() {
-  return <div>
-    <img src="imgs/prosciutto.jpg" alt='prosciutto' />
-    <h2>Pizza Prosciutto</h2>
-    <p>Prosciutto, Italian sausage, onions, bacon, pepperoni</p>
-  </div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
